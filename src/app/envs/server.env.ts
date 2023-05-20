6055a8c50728d67c48ba4bccf9ae9edc47efb2ed
LucasMachadoG <lucasgaelzermachado@gmail.com>
2023-05-20 09:42:59 -0300

    Configuracoes do projeto api-vagas

---
+++ src/app/envs/server.env.ts
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export const ServerEnv = {
+    port: process.env.PORT
+}
---
+++ src/main/server/express.server.ts
@@ -0,0 +1,12 @@
+import { ServerEnv } from "../../app/envs/server.env";
+import { createApp } from "../config/express.config";
+
+export class Server {
+    public static run () {
+        const app = createApp()
+
+        app.listen (ServerEnv.port, () => {
+            console.log("A API esta rodando...")
+        })
+    }
+}
